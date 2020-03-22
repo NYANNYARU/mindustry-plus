e4f0895da8d7c73f79dcfbe54e1ea359d266d980
NYANNYARU <59269549+NYANNYARU@users.noreply.github.com>
2020-03-22 13:37:09 +0900

    Create fuse_bullet.java

---
+++ fuse_bullet.java
@@ -0,0 +1,66 @@
+package mindustry.content;
+
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.util.*;
+import mindustry.ctype.ContentList;
+import mindustry.entities.*;
+import mindustry.entities.bullet.*;
+import mindustry.entities.effect.*;
+import mindustry.entities.type.*;
+import mindustry.graphics.*;
+import mindustry.world.*;
+
+fuse = new ItemTurret("fuse"){{
+            requirements(Category.turret, ItemStack.with(Items.copper, 225, Items.graphite, 225, Items.thorium, 100));
+
+            reload = 35f;
+            shootShake = 4f;
+            range = 90f;
+            recoil = 5f;
+            shots = 3;
+            spread = 20f;
+            restitution = 0.1f;
+            shootCone = 30;
+            size = 3;
+
+            health = 220 * size * size;
+            shootSound = Sounds.shotgun;
+
+            ammo(Items.graphite, new BulletType(0.01f, 105){
+                int rays = 1;
+                float rayLength = range + 10f;
+
+                {
+                    hitEffect = Fx.hitLancer;
+                    shootEffect = smokeEffect = Fx.lightningShoot;
+                    lifetime = 10f;
+                    despawnEffect = Fx.none;
+                    pierce = true;
+                }
+
+                @Override
+                public void init(mindustry.entities.type.Bullet b){
+                    for(int i = 0; i < rays; i++){
+                        Damage.collideLine(b, b.getTeam(), hitEffect, b.x, b.y, b.rot(), rayLength - Math.abs(i - (rays / 2)) * 20f);
+                    }
+                }
+
+                @Override
+                public void draw(Bullet b){
+                    super.draw(b);
+                    Draw.color(Color.white, Pal.lancerLaser, b.fin());
+                    //Draw.alpha(b.fout());
+                    for(int i = 0; i < 7; i++){
+                        Tmp.v1.trns(b.rot(), i * 8f);
+                        float sl = Mathf.clamp(b.fout() - 0.5f) * (80f - i * 10);
+                        Drawf.tri(b.x + Tmp.v1.x, b.y + Tmp.v1.y, 4f, sl, b.rot() + 90);
+                        Drawf.tri(b.x + Tmp.v1.x, b.y + Tmp.v1.y, 4f, sl, b.rot() - 90);
+                    }
+                    Drawf.tri(b.x, b.y, 20f * b.fout(), (rayLength + 50), b.rot());
+                    Drawf.tri(b.x, b.y, 20f * b.fout(), 10f, b.rot() + 180f);
+                    Draw.reset();
+                }
+            });
+        }};
